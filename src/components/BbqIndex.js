510e1059728b82687d2c12935940a276d82b2631
github username <72711695+victoriaehrbar@users.noreply.github.com>
2022-03-04 17:04:59 -0600

    bbq index

--- src/components/BbqIndex.js
+++ src/components/BbqIndex.js
@@ -1,9 +1,28 @@
 import React from "react";
+import { connect } from "react-redux";
+import { getBbqs } from "../redux/actions/bbqActions";
+import BbqListItem from "./BbqListItem";
 
-export default function BbqIndex() {
-  return (
-    <div>
-      <h1>Bbq Index</h1>
-    </div>
-  );
+class BbqIndex extends React.Component {
+  componentDidMount() {
+    this.props.getBbqs();
+  }
+  render() {
+    return (
+      <div>
+        <h1>BBQs</h1>
+        {this.props.bbqs.map((bbq) => (
+          <BbqListItem bbq={bbq} />
+        ))}
+      </div>
+    );
+  }
 }
+
+const mapStateToProps = ({ bbqs }) => {
+  return {
+    bbqs: bbqs.all,
+  };
+};
+
+export default connect(mapStateToProps, { getBbqs })(BbqIndex);

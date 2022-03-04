a1eb25b8d79d64eca084689f9a371ae6ed421bab
github username <72711695+victoriaehrbar@users.noreply.github.com>
2022-03-04 16:12:58 -0600

    app.js

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Home from "./components/Home";
+import BbqForm from "./components/BbqForm";
+import BbqIndex from "./components/BbqIndex";
+import Nav from "./components/Nav";
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <Nav />
+        <Switch>
+          <Route exact path="/" component={Home} />
+          <Route exact path="/bbqs" component={BbqIndex} />
+          <Route path="/bbqs/new" component={BbqForm} />
+        </Switch>
+      </Router>
     </div>
   );
 }
